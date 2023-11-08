373def87ede5936c8d3207ce681a88e4cc227b5d
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2023-11-08 22:35:17 +0200

    Adding CSS

--- script.js
+++ script.js
@@ -17,11 +17,36 @@ function updateTaskList() {
 
     tasks.forEach(function (task, i) {
         const li = document.createElement('li');
+
+        li.appendChild(document.createTextNode(task.name));
+
+        const taskDiv = document.createElement('div');
+
         const checkbox = document.createElement('input');
         checkbox.type = 'checkbox';
         checkbox.checked = task.completed;
         checkbox.addEventListener('change', function () {
             task.completed = checkbox.checked;
+        });
+
+        const editButton = document.createElement('button');
+        editButton.textContent = 'Edit';
+        editButton.addEventListener('click', function () {
+            newName.style.display = 'inline';
+            newName.focus();
+            editButton.style.display = 'none';
+            saveButton.style.display = 'inline';
+        });
+
+        const saveButton = document.createElement('button');
+        saveButton.textContent = 'Save';
+        saveButton.style.display = 'none';
+        saveButton.addEventListener('click', function () {
+            task.name = newName.value;
+            newName.style.display = 'none';
+            editButton.style.display = 'inline';
+            saveButton.style.display = 'none';
+            updateTaskList();
         });
 
         const deleteButton = document.createElement('button');
@@ -35,28 +60,14 @@ function updateTaskList() {
         newName.type = 'text';
         newName.value = task.name;
 
-        const editButton = document.createElement('button');
-        editButton.textContent = 'Edit';
-        editButton.addEventListener('click', function () {
-            newName.style.display = 'inline';
-            newName.focus();
-            task.name = newName.value;
-            newName.style.display = 'none';
-            taskList.removeChild(li);
-            tasks.splice(i, 1);
-            tasks.splice(i, 0, { name: newName.value, completed: task.completed });
-            updateTaskList();
-        });
+        taskDiv.appendChild(checkbox);
+        taskDiv.appendChild(newName);
+        taskDiv.appendChild(editButton);
+        taskDiv.appendChild(saveButton);
+        taskDiv.appendChild(deleteButton);
 
-        li.appendChild(checkbox);
-        li.appendChild(newName);
-        li.appendChild(editButton);
-        li.appendChild(deleteButton);
-        li.appendChild(document.createTextNode(task.name)); // Display the task name
-
+        li.appendChild(taskDiv);
         taskList.appendChild(li);
     });
 }
-
-// Initialize the task list
 updateTaskList();

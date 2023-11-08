c1f85cd4140e8250886d6d79ec89dc17fb661024
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2023-11-08 21:41:11 +0200

    Adding Edit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const taskList = document.getElementById('taskList');
 
 addTask.addEventListener('click', function () {
     const taskName = taskInput.value;
-    if (taskName.trim() !== '') { 
+    if (taskName.trim() !== '') {
         tasks.push({ name: taskName, completed: false });
         taskInput.value = '';
         updateTaskList();
@@ -21,7 +21,7 @@ function updateTaskList() {
         checkbox.type = 'checkbox';
         checkbox.checked = task.completed;
         checkbox.addEventListener('change', function () {
-            tasks[i].completed = checkbox.checked;
+            task.completed = checkbox.checked;
         });
 
         const deleteButton = document.createElement('button');
@@ -31,9 +31,32 @@ function updateTaskList() {
             updateTaskList();
         });
 
-        li.textContent = task.name;
+        const newName = document.createElement('input');
+        newName.type = 'text';
+        newName.value = task.name;
+
+        const editButton = document.createElement('button');
+        editButton.textContent = 'Edit';
+        editButton.addEventListener('click', function () {
+            newName.style.display = 'inline';
+            newName.focus();
+            task.name = newName.value;
+            newName.style.display = 'none';
+            taskList.removeChild(li);
+            tasks.splice(i, 1);
+            tasks.splice(i, 0, { name: newName.value, completed: task.completed });
+            updateTaskList();
+        });
+
         li.appendChild(checkbox);
+        li.appendChild(newName);
+        li.appendChild(editButton);
         li.appendChild(deleteButton);
+        li.appendChild(document.createTextNode(task.name)); // Display the task name
+
         taskList.appendChild(li);
     });
 }
+
+// Initialize the task list
+updateTaskList();
